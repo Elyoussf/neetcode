1512a8856e01deac2bf3e01761433aed692111f5
Elyoussf <strangeamazigh1968@gmail.com>
2024-11-10 20:55:52 +0100

    just relax it's me

--- DP/LengthLongestSubstring.py
+++ DP/LengthLongestSubstring.py
@@ -1,18 +1,21 @@
 from typing import *
-
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n = len(text1)
-        m = len(text2)
-        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]  # Initialize dp array with (m+1)x(n+1) size
-        
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if text1[j - 1] == text2[i - 1]:  # Match found
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
-                else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        
-        return dp[m][n]  # The answer is in the bottom-right corner of dp matrix
+    def maxProfit(self, prices: List[int]) -> int:
+        #sell at day ith  cannot buy at day i+1
+        n = len(prices)
+        dp = [0 for _ in range(n+1)]  # dp[i] maximum profit could be gained at day i 
+        # before caluculation i suppose i did not do any activity yet.
+        #so:
+        dp[0] = 0
+        for i in range(1,n+1):
+            if dp[i-1] >0 : # sold on the last day  i cannot buy 
+                continue
+            if dp[i-1] <0 : 
+                dp[i] = max(dp[i-1]+prices[i-1],0)
+                continue
+            dp[i] = -prices[i-1] # bought , lost money so (-)
 
-
+        return dp
+s = Solution()
+print(s.maxProfit([1,3,4,0,4]))
+        
