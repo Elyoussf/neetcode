8686a47110789a2432913f5f0ee64ab0e1fb712a
Elyoussf <strangeamazigh1968@gmail.com>
2025-01-14 02:05:25 +0100

    Chilling in Bitet

--- DP/uncrossedLines.py
+++ DP/uncrossedLines.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        return self.solve(len(nums1),len(nums2),nums1,nums2,{})
+        return self.func(nums1,nums2)
 
     def solve(self,i,j,nums1,nums2,memo):
         if (i,j) in memo:
@@ -15,3 +15,22 @@ class Solution:
         else:
             memo[(i,j)] =  max(self.solve(i-1,j,nums1,nums2,memo),self.solve(i,j-1,nums1,nums2,memo)) 
         return memo[(i,j)]
+    
+
+    def func(self,nums1,nums2):
+        n1 = len(nums1)
+        n2 = len(nums2)
+        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+       
+        
+        for i in range(1,n1+1):
+            for j in range(1,n2+1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+        return dp[n1][n2]
+
+s = Solution()
+print(s.maxUncrossedLines([1,4,2],[1,2,4]))
+        
